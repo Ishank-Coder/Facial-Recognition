17da330972d337010635f8594f6438b20ce04c0a
Ishank-Coder <ishuishank18@gmail.com>
2023-10-13 18:31:19 +0530

    okok

---
+++ test.py
@@ -0,0 +1,7 @@
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.preprocessing.image import load_img
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+import os
+import pandas as pd
+import numpy as np
